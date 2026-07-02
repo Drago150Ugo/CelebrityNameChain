9af856e259ad3ee04fe2a622cdcaec435638d959
zaraaudri <zaratasnim05@gmail.com>
2026-07-02 12:12:47 -0400

    home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonText, IonList, IonLabel, IonItem } from '@ionic/react';
 import "./Home.css";
 const Home: React.FC = () => {
   return (
@@ -15,9 +15,29 @@ const Home: React.FC = () => {
       <IonButton 
       expand="block"
       size="large"    
+      color="danger"
       routerLink="/Lobby">    
-        SET LOBBY
+        LOBBY
       </IonButton>
+      <IonText>
+    <h3>How To Play</h3>
+      </IonText>
+              <IonList>
+            <IonItem lines="none">
+              <IonLabel>Start with a celebrity.</IonLabel>
+            </IonItem>
+
+            <IonItem lines="none">
+              <IonLabel>
+                Name another celebrity connected by the last name.
+              </IonLabel>
+            </IonItem>
+
+            <IonItem lines="none">
+              <IonLabel>Don't repeat names.</IonLabel>
+            </IonItem>
+          </IonList>
+
       </div>
       </IonContent>
     </IonPage>
