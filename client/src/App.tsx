eb1ca95d009fe56fb29443867cf7ae139da8a571
zaraaudri <zaratasnim05@gmail.com>
2026-07-02 12:45:44 -0400

    guess.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import Lobby from './pages/Lobby';
+import Guess from './pages/guess';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -46,6 +47,9 @@ const App: React.FC = () => (
         </Route>
         <Route exact path="/lobby">
           <Lobby />
+        </Route>
+        <Route exact path="/guess">
+          <Guess/>
         </Route>
         <Route exact path="/">
           <Redirect to="/home" />
--- client/src/pages/Lobby.tsx
+++ client/src/pages/Lobby.tsx
@@ -1,20 +1,24 @@
 import { IonButton, IonContent, IonHeader, IonInput, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-
+import "./Home.css";
 const Lobby: React.FC = () => {
   return (
     <IonPage>
-        <IonTitle>
-            Lobby
-        </IonTitle>
         <IonContent fullscreen>
-            <p>Welcome to the lobby!</p>
-        </IonContent>
-        <IonList>
+             <IonTitle className="ion-text-center"> 
+            Welcome To The Lobby 
+            </IonTitle>
+              <IonList>
             <IonInput placeholder="Enter your name" />
             <IonInput placeholder="Enter room code" />
-            <IonButton>Join Room</IonButton>
-            <IonButton>Create Room</IonButton>  
+            <IonButton
+            color="danger"
+            routerLink="/Guess">  
+            Join Room</IonButton>
+            <IonButton       
+            color="danger">
+            Create Room</IonButton>  
         </IonList>
+        </IonContent>
     </IonPage>
   );
 }
