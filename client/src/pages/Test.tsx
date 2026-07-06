c3b80afcf4be76d904630b253f7a0ddb4ef0687b
Drago150Ugo <u.b.e900@outlook.com>
2026-07-06 09:44:52 -0400

    Lobby react hook form in progress

---
+++ client/src/pages/Test.tsx
@@ -0,0 +1,14 @@
+import {
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import { useQuery } from '@tanstack/react-query';
+
+const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000';
+
