a22fde6c30a5c40f430c9661cf65cf1fccfd3036
AbuTalha63 <usmanm6390@gmail.com>
2026-07-07 16:10:02 -0400

    successful connection between frontend react and backend express API server
    -only the health route works currently, and shows the result in the button itself, however, the connection is established given both servers are online

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT ?? 3000;
 
